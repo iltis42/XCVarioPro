c49ea72159152768b9b27c7af62d71c9ff3a0a4a
Helmut J. Rohs <helmut.j.rohs@web.de>
2025-11-10 08:19:00 +0100

    Enlarged 10km/h flap speed dots.

--- main/screen/element/FlapsBox.cpp
+++ main/screen/element/FlapsBox.cpp
@@ -180,8 +180,8 @@ void FlapsBox::draw(float ias)
     if ( _dirty ) {
         MYUCG->setColor(COLOR_HEADER);
         MYUCG->drawRFrame(_ref_x, _ref_y-BOX_LENGTH/2-BOX_CORNER, BOX_WIDTH, BOX_LENGTH + 2*BOX_CORNER, BOX_CORNER);
-        MYUCG->drawDisc(_ref_x, _ref_y + 10*PIX_PER_KMH, 2, UCG_DRAW_ALL);
-        MYUCG->drawDisc(_ref_x, _ref_y - 10*PIX_PER_KMH, 2, UCG_DRAW_ALL);
+        MYUCG->drawDisc(_ref_x, _ref_y + 10*PIX_PER_KMH, 3, UCG_DRAW_ALL);
+        MYUCG->drawDisc(_ref_x, _ref_y - 10*PIX_PER_KMH, 3, UCG_DRAW_ALL);
         MYUCG->setColor(COLOR_WHITE);
         MYUCG->drawDisc(_ref_x, _ref_y, 3, UCG_DRAW_ALL);
     }
